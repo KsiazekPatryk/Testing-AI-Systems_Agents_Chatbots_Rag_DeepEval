7aeb8c6674f538e27115ab590f91841d594b0111
Patryk <ksiazekpatryk@gmail.com>
2026-06-29 19:56:21 +0200

    Refactor test case for task completion to store actual output and evaluate metrics

--- DeepEval-Agent/test_TaskCompletion.py
+++ DeepEval-Agent/test_TaskCompletion.py
@@ -1,9 +1,14 @@
 from deepeval.test_case import LLMTestCase
+from deepeval import evaluate
 from agent_instrumented import support_agent
+import deepeval.metrics as TaskCompletionMetric
 
-support_agent("Where is my ordder ORD-1042?")
+actual_output = support_agent("Where is my ordder ORD-1042?")
 
-LLMTestCase(
+test_case = LLMTestCase(
     input = "Where is my order ORD-1042?",
-    actual_output = ""
+    actual_output = actual_output
 )
+
+evaluate(test_cases = [test_case],
+         metrics = [TaskCompletionMetric.TaskCompletionMetric(threshold=0.7, model="claude-sonnet-4-6")])
